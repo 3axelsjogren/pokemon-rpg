e364c589d60afd23dc1e10a0600574a4b90c8baf
Axel Sjogren <din@email.com>
2026-05-12 16:48:29 +0200

    fixat hållbarare typomvandlingar med static_cast

--- src/MapManager.cpp
+++ src/MapManager.cpp
@@ -40,6 +40,30 @@ static const std::vector<std::vector<int>> HOUSE_MAP = {
     { 4, 4, 4, 6, 4, 4, 4, 4 },  // 6 = dörr ut, 4 = väggar, 7 = golv
 };
 
+// Utomhuskartan
+static const std::vector<std::vector<int>> FIRST_FILLER_MAP = { // TODO: rita om nytt landksap med bro och flod, bara filler
+    { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 2, 2, 0, 0, 0, 4, 4, 4, 4, 0, 0, 2, 2, 0, 0, 0, 2 },
+    { 2, 0, 0, 2, 2, 0, 0, 0, 4, 4, 6, 4, 0, 0, 2, 2, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 1, 1, 1, 1, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 2 },
+    { 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 2 },
+    { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+};
+
 MapManager::MapManager() {
     RegisterMaps();
     SwitchMap("outdoor", 10 * TILE_SIZE, 10 * TILE_SIZE);
@@ -47,7 +71,8 @@ MapManager::MapManager() {
 
 void MapManager::RegisterMaps() {
     m_mapData["outdoor"] = OUTDOOR_MAP;
-    m_mapData["house"]   = HOUSE_MAP;
+    m_mapData["house"] = HOUSE_MAP;
+    m_mapData["woods_first"] = FIRST_FILLER_MAP; // TODO: utveckla stig till vänster för första scen-switchen till äventyret
 
     // Dörr på utomhuskartan: tile (10,4) leder till house
     m_doors["outdoor"].push_back({ 10, 4, "house", 3 * TILE_SIZE, 5 * TILE_SIZE });
@@ -61,16 +86,16 @@ void MapManager::RegisterMaps() {
 
 void MapManager::SwitchMap(const std::string& mapName, float spawnX, float spawnY) {
     auto& data = m_mapData[mapName];
-    int w = (int)data[0].size();
-    int h = (int)data.size();
+    int w = static_cast<int>(data[0].size());
+    int h = static_cast<int>(data.size());
     m_currentMap = std::make_unique<TileMap>(w, h);
     m_currentMap->LoadFromArray(data);
     m_currentMapName = mapName;
 }
 
 bool MapManager::CheckDoorTrigger(float playerX, float playerY, float& outSpawnX, float& outSpawnY, std::string& outTargetMap) {
-    int tileX = (int)(playerX / TILE_SIZE);
-    int tileY = (int)(playerY / TILE_SIZE);
+    int tileX = static_cast<int>(playerX / TILE_SIZE);
+    int tileY = static_cast<int>(playerY / TILE_SIZE);
 
     for (auto& door : m_doors[m_currentMapName]) {
         if (door.tileX == tileX && door.tileY == tileY) {
