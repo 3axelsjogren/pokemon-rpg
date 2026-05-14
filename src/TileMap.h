1d8cb8b9f03c1b954bc56fb5393b5be73ac59fd0
Axel Sjogren <din@email.com>
2026-05-14 14:00:57 +0200

    adderat grott-öppning

--- src/TileMap.h
+++ src/TileMap.h
@@ -6,6 +6,7 @@
 
 // Tile IDs
 enum TileType {
+    // outside/houses
     TILE_GRASS = 0,
     TILE_WATER = 1,
     TILE_TREE = 2,
@@ -15,6 +16,12 @@ enum TileType {
     TILE_DOOR = 6,
     TILE_FLOOR = 7,
     TILE_LOCKED_DOOR = 8,
+    TILE_LEVER = 9, // framtida användning
+
+    // Caves
+    TILE_CAVE_WALL = 10,
+    TILE_CAVE_FLOOR = 11,
+    TILE_GEM = 12,
 };
 
 struct Tile {
